16be1bc03f9a81bf853ac870b77c664c994ab90d
Latch Jackn <latch.jack@gmail.com>
2022-11-24 22:56:23 +0000

    Working on Dashboard cards

--- src/pages/dashboard-page.jsx
+++ src/pages/dashboard-page.jsx
@@ -4,17 +4,19 @@ import Layout from '../ui/layout/layout';
 function DashboardPage() {
   return (
     <Layout>
-      <div className="mb-2 border border-black min-w-xl max-w-3xl h-52">
-        Last workout card
-      </div>
-      <div className=" mb-2 border border-black min-w-xl max-w-3xl h-52">
-        Today's workout card
-      </div>
-      <div className="mb-2 border border-black min-w-xl max-w-3xl h-52">
-        PR card
-      </div>
-      <div className="mb-2 border border-black min-w-xl max-w-3xl h-52">
-        Trophy cabinet
+      <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-3 gap-4">
+        <div className="border border-black min-w-xl max-w-3xl h-52">
+          Last workout card
+        </div>
+        <div className="border border-black min-w-xl max-w-3xl h-52">
+          Today's workout card
+        </div>
+        <div className="border border-black min-w-xl max-w-3xl h-52">
+          PR card
+        </div>
+        <div className="border border-black min-w-xl max-w-3xl h-52">
+          Trophy cabinet
+        </div>
       </div>
     </Layout>
   );
