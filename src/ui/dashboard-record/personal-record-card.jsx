b86f011dfaf0e630777a8f827f734d66616108af
Latch Jackn <latch.jack@gmail.com>
2022-11-25 00:00:21 +0000

    Working on Dashboard cards

--- src/ui/dashboard-record/personal-record-card.jsx
+++ src/ui/dashboard-record/personal-record-card.jsx
@@ -4,8 +4,18 @@ import { RocketLaunchIcon } from '@heroicons/react/20/solid';
 const PersonalRecordCard = () => {
   return (
     <div className="border border-red-600 rounded-lg min-w-xl max-w-3xl h-52 relative  p-4 px-6">
-      <p className="text-xl text-indigo-600">Most Recent Personal Record</p>
-      <div></div>
+      <p className="text-xl text-indigo-600">Most Recent Personal Records</p>
+      <ul>
+        <li className="text-sm flex gap-3">
+          <span>Bench Press</span>|<span>80kg</span>|<span>5 x 3</span>
+        </li>
+        <li className="text-sm flex gap-3">
+          <span>Squat</span>|<span>120kg</span>|<span>5 x 3</span>
+        </li>
+        <li className="text-sm flex gap-3">
+          <span>Dead Lift</span>|<span>130kg</span>|<span>5 x 3</span>
+        </li>
+      </ul>
       <div></div>
       <RocketLaunchIcon
         className="h-36 text-indigo-600 absolute right-12"
--- src/ui/dashboard-workout/todays-workout-card.jsx
+++ src/ui/dashboard-workout/todays-workout-card.jsx
@@ -6,7 +6,26 @@ const WorkoutTodayCard = () => {
   return (
     <div className="border border-purple-600 rounded-lg min-w-xl max-w-3xl h-52 relative  p-4 px-6">
       <p className="text-xl text-indigo-600">Today's workout card</p>
-      <div></div>
+      <div>
+        <p>Its: Leg Day!</p>
+        <ul>
+          <li className="text-sm flex gap-3">
+            <span>Squat</span>|<span>5 x 3</span>
+          </li>
+          <li className="text-sm flex gap-3">
+            <span>Leg Press</span>|<span>5 x 3</span>
+          </li>
+          <li className="text-sm flex gap-3">
+            <span>Goblet Squat</span>|<span>5 x 3</span>
+          </li>
+          <li className="text-sm flex gap-3">
+            <span>Ham Curl</span>|<span>5 x 3</span>
+          </li>
+          <li className="text-sm flex gap-3">
+            <span>Leg Extension</span>|<span>5 x 3</span>
+          </li>
+        </ul>
+      </div>
       <div></div>
       <BoltIcon
         className="h-36 text-indigo-600 absolute right-12"
